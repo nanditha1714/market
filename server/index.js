a5ba1243b2c79c76026817544f800665d8357dd0
nanditha1714 <nandithane17@gmail.com>
2026-04-16 10:06:10 +0530

    fix: updated express wildcard for express 5 compatibility

--- server/index.js
+++ server/index.js
@@ -102,7 +102,7 @@ if (process.env.NODE_ENV === 'production') {
   const buildPath = path.join(__dirname, '../react-app/build');
   app.use(express.static(buildPath));
   
-  app.get('(.*)', (req, res) => {
+  app.get('/:path*', (req, res) => {
     res.sendFile(path.join(buildPath, 'index.html'));
   });
 }
